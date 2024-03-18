dc4c3f0b68317975d2b58511d09fe8ab5e0513e7
Moshrur Ahmad <moshrurahmad@gmail.com>
2024-03-18 10:05:24 +0600

    Two constructor method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
+    public int roll;
+    public String name;
+    public boolean passed;
+    public HelloWorld(){
+        roll=100;
+        name="CSE";
+        passed=true;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        HelloWorld helloWorld=new HelloWorld();
+        System.out.println(helloWorld.name);
+        System.out.println(helloWorld.roll);
     }
 }
